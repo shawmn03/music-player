b41b03de802f5d4fa7754098f2b21b1ecc3b36c3
Megan Shaw <shawmn@vcu.edu>
2025-04-29 16:41:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const songs = [
   'songs/ABBA - The Winner Takes It All.mp3',
   'songs/Conan Gray - Never Ending Song.mp3',
   'songs/Taylor Swift - The Prophecy.mp3',
-  'songs/song4.mp3',
+  'songs/Gotye - Somebody That I Used To Know.mp3',
   'songs/song5.mp3',
   'songs/song6.mp3',
 ];
